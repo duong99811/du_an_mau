463946e2f681cf8c75bd09bb6529b1a608641f49
duong dien <duongh2so4@gmail.com>
2020-10-26 00:57:07 +0700

    update

--- app/src/main/java/com/e/du_an_mau/sqLite/HoaDonDAO.java
+++ app/src/main/java/com/e/du_an_mau/sqLite/HoaDonDAO.java
@@ -14,6 +14,7 @@ import java.util.Date;
 import java.util.List;
 
 public class HoaDonDAO {
+
     MySqlite mySqlite;
     SQLiteDatabase sqLiteDatabase;
     SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
